7303b3260274e66c127e826479e2cdac4316d8de
Miguel Garcia <angelito.garcia1993@gmail.com>
2024-03-15 18:59:03 -0600

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi Primer Tarea",
+    description: "Mi Primer Tarea",
+  },
+  {
+    id: 1,
+    title: "Mi Primer Tarea 1",
+    description: "Mi Primer Tarea 1",
+  },
+  {
+    id: 2,
+    title: "Mi Primer Tarea 2",
+    description: "Mi Primer Tarea 2",
+  },
+  {
+    id: 3,
+    title: "Mi Primer Tarea 3",
+    description: "Mi Primer Tarea 3",
+  },
+];
